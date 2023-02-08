fba52093e74cd766e49ee2052452ad279d28c294
Nashath Nasik <mail2snasik@gmail.com>
2023-02-08 18:44:05 +0530

    bug fix

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -16,27 +16,17 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
-<!-- Ionicons -->
-<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<!-- Tempusdominus Bootstrap 4 -->
-<link rel="stylesheet" href="{{asset('plugins/bootstrap/css/bootstrap.min.css')}}">
-<!-- iCheck -->
-<link rel="stylesheet" href="{{asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+
 <!-- JQVMap -->
 <link rel="stylesheet" href="{{asset('plugins/jqvmap/jqvmap.min.css')}}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
 <!-- overlayScrollbars -->
 <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
-<!-- Daterange picker -->
-<link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker.css')}}">
-<!-- summernote -->
-<link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
+
+<!-- Tempusdominus Bootstrap 4 -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
 <!-- Custom CSS-->
 <link rel="stylesheet" href="{{asset('css/app.css')}}">
-
-<!-- Custom APP JS -->
-<script src="{{asset('js/app.js')}}"></script>
-
 
